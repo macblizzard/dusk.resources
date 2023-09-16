dfe842eedffaec40483ba9abcf32be4ecaa70221
macblizzard <dna.mailbox@gmail.com>
2023-09-16 10:09:28 +0200

    Update status_updater.sh

--- scripts/statusbar/status_updater.sh
+++ scripts/statusbar/status_updater.sh
@@ -1,63 +1,37 @@
-#!/bin/bash
-
-DIR=$(dirname ${BASH_SOURCE[0]})
+#!/bin/sh
 
 for pid in $(pidof -x "status_updater.sh"); do
-	if [ $pid != $$ ]; then
-		if [ "$(cat /proc/$pid/environ | tr '\0' '\n' | awk '/^DISPLAY/')" == "DISPLAY=$DISPLAY" ]; then
-			echo "$(date +"%F %T"): status_updater.sh is already running with PID $pid, killing"
-			kill $pid
-		fi
-	fi
+    if [ $pid != $$ ]; then
+        echo "$(date +"%F %T"): status_updater.sh is already running with PID $pid, killing"
+        kill $pid
+    fi
 done
 
-wait_for_ipc_handler () {
-	seconds=$1
-
-	for (( d=1; d<=2*${seconds}; d++ ))
-	do
-		sleep 0.5
-		duskc --ignore-reply get_monitors 2>/dev/null 1>&2
-		if [ $? = 0 ]; then
-			return
-		fi
-	done
-
-	echo "Failed to reach IPC handler, bailing"
-	exit
-}
+# Add an artificial sleep to wait for the IPC handler to be ready to process requests
+sleep 0.5
 
 SETSTATUS="duskc --ignore-reply run_command setstatus"
-secs=0
 
+$SETSTATUS 7 “$(~/.local/bin/statusbar/statusbutton)" &
+
+secs=0
 while true; do
 
-	$SETSTATUS 0 "$($DIR/clock)"
+    $SETSTATUS 0 "$(~/.local/bin/statusbar/clock)" &
+    $SETSTATUS 1 "$(~/.local/bin/statusbar/weather)” &
+    $SETSTATUS 2 "$(~/.local/bin/statusbar/mem)” &
+    $SETSTATUS 3 "$(~/.local/bin/statusbar/cpu)” &
 
-	if [ $? != 0 ]; then
-		wait_for_ipc_handler 30
-	fi
 
-	$SETSTATUS 2 "$($DIR/mem)" &
-	$SETSTATUS 3 "$($DIR/cpu)" &
+#    if [ $((secs % 60)) = 0 ]; then
+#       $SETSTATUS 5 "$(~/.local/bin/statusbar/mouse_battery)" &
+#        $SETSTATUS 1 "$(~/.local/bin/statusbar/volume)" &
+#    fi
 
-	if [ $((secs % 60)) = 0 ]; then
-		$SETSTATUS 5 "$($DIR/mouse_battery)" &
-		$SETSTATUS 1 "$($DIR/volume)" &
-		$SETSTATUS 9 "$($DIR/kblayout)" &
-	fi
+#    if [ $((secs % 3600)) = 0 ]; then
+#       $SETSTATUS 4 "$(~/.local/bin/statusbar/sysupdates_paru)" &
+#    fi
 
-	# Update these once after 1 minute, i.e. hourly
-	if [ $secs = 60 ]; then
-		$SETSTATUS 4 "$($DIR/sysupdates)" &
-		$SETSTATUS 7 "$($DIR/statusbutton)" &
-	fi
-
-	# Restart script every hour
-	if [ $secs = 3600 ]; then
-		exec ${BASH_SOURCE[0]}
-	fi
-
-	((secs+=1))
-	sleep 1
+    ((secs+=1))
+    sleep 1
 done
